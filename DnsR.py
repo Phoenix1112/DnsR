5d596ee82af72c622771b4a82ab828d9c79dedae
Phoenix1112 <42475284+Phoenix1112@users.noreply.github.com>
2020-02-22 16:16:02 +0300

    Update DnsR.py

--- DnsR.py
+++ DnsR.py
@@ -2,127 +2,138 @@ import os
 import sys
 import pydig
 import argparse
-import tldextract
+import threading
 from colorama import *
-from multiprocessing import Pool, cpu_count
+from concurrent.futures import ThreadPoolExecutor
+
+init(autoreset=True)
 
 def attack(sub):
 
-	querys = pydig.query(str(sub),'A')
-	
-	if not querys:
-		querys = pydig.query(str(sub),'CNAME')
+    try:
 
-	if len(querys) > 0:
-		if args["blacklist"]:
-			total = 0
+        query_A = pydig.query(sub, "A")
 
-			if args["blacklist"].count(",") > 0:
-				parse = args["blacklist"].split(",")
+        if query_A:
 
-				for black in parse:
-					for zor in querys:
-						if black in zor:
-							total += 1
-						else:
-							pass
-			else:
-				for zor in querys:
-					if args["blacklist"] in zor:
-						total += 1
-					else:
-						pass
+            if args.output:
+                save_output(sub)
 
-			if total == 0:
-				if args["output"]:
-					PrintNow(sub)
-				print(sub)
-			else:
-				pass
-		else:
-			if args["output"]:
-				PrintNow(sub)
-			print(sub)
-	else:
-		pass
+            with print_lock:
+                print(Fore.GREEN+sub)
 
+        else:
 
+            query_cname = pydig.query(sub, "CNAME")
 
-def PrintNow(sub):
-	with open(args["output"],"a+",encoding="utf-8") as file:
-		file.write(str(sub)+"\n")
+            if query_cname:
 
+                if args.output:
+                    save_output(sub)
+
+                with print_lock:
+                    print(Fore.GREEN+sub)
+
+    except:
+        pass
+
+def save_output(x):
+
+    with open(args.output, "a+", encoding="utf-8") as f:
+        f.write(str(x) + "\n")
 
 
 def main():
 
-	SubList = list()
-	blacklist = ("<",">",",",".","%","&","_","-","|","*","$","é","^","#","/","~","+","(",")","{","}","[","]",";",":")
+    target_list = list()
 
-	#Subdomain stdin den alıyoruz
-	for Subdomain in sys.stdin:
-		Subdomain = str(Subdomain.rstrip()).lower()
+    if args.list:
 
-	#Subdomaini kontrol ediyoruz ve içindeki negatif karakterlere göre filitreliyoruz
-		if Subdomain:
+        if not os.path.exists(args.list):
+            print(Fore.RED+"Target List Not Found: {}".format(args.list))
+            sys.exit()
 
-			tld = tldextract.extract(Subdomain)
-			tld = tld.subdomain
+        print(banner)
 
-			if len(tld) > 63 and not "." in tld:
-				pass
+        with open(args.list, "r", encoding="utf-8") as f:
+            [target_list.append(x) for x in f.read().lower().split("\n") if x and black_character(x)]
 
-			else:
-				if not Subdomain.startswith(blacklist):
+    elif args.stdin:
 
-					if Subdomain.find("..") == -1 and Subdomain.find("--") == -1 and Subdomain.find("*") == -1\
-					and Subdomain.find("?") == -1 and Subdomain.find(":") == -1 and Subdomain.find(";") == -1\
-					and Subdomain.find("%") == -1 and Subdomain.find("&") == -1 and Subdomain.find("$") == -1\
-					and Subdomain.find("-.") == -1 and Subdomain.find(".-") == -1:
+        [target_list.append(x) for x in sys.stdin.read().lower().split("\n") if x and black_character(x)]
 
-						sayı = 0
-						whitelist = ["q","w","e","r","t","y","u","o","p","a","s","d","f","g","h","j","k","l","i",
-						"z","x","c","v","b","n","m","0","1","2","3","4","5","6","7","8","9",".","_","-"]
+    else:
 
-						for whitekey in Subdomain:
-							if not str(whitekey) in whitelist:
-								sayı += 1
-							else:
-								pass
-						if sayı == 0:
-							SubList.append(Subdomain)
-						else:
-							pass
+        print(Fore+RED+"You Need To Use --list Or --stdin Param")
+        sys.exit()
 
-					else:
-						pass
-				else:
-					pass
-		else:
-			pass
 
-	with Pool(cpu_count()) as p:
-		p.map(attack,SubList)
+    if target_list:
+
+        with ThreadPoolExecutor(max_workers=args.thread) as executor:
+            for sub in target_list:
+                executor.submit(attack, sub)
+
+    else:
+
+        print(Fore.RED+"Not Found Any Target")
+        sys.exit()
+
+def black_character(subdomain):
+
+    try:
+
+        character_list = [".-", "-.", "&", "%", "(", ")", "$", "#", "<", ">", "/", "*",
+        "?", "~", ",", ";", ":", "|", "{", "}", "[","]", "^", "'", "+"]
+
+        sayı = 0
+
+        for black in character_list:
+
+            if black in subdomain:
+
+                sayı += 1
+                break
+
+            else:
+                pass
+
+        if sayı == 0:
+
+            return True
+
+        else:
+
+            return False
+
+    except:
+
+        return False
+
+
 
 if __name__ == "__main__":
 
-	if os.name == "nt":
-		init(convert=True)
+    banner = Fore.CYAN+r"""
+    _______                       _______
+    |       \                     |       \
+    | $$$$$$$\ _______    _______ | $$$$$$$\
+    | $$  | $$|       \  /       \| $$__| $$
+    | $$  | $$| $$$$$$$\|  $$$$$$$| $$    $$
+    | $$  | $$| $$  | $$ \$$    \ | $$$$$$$\
+    | $$__/ $$| $$  | $$ _\$$$$$$\| $$  | $$
+    | $$    $$| $$  | $$|       $$| $$  | $$
+     \$$$$$$$  \$$   \$$ \$$$$$$$  \$$   \$$
+    
+    """
 
-	banner = Fore.CYAN+r"""
-	 _______                       _______
-	|       \                     |       \
-	| $$$$$$$\ _______    _______ | $$$$$$$\
-	| $$  | $$|       \  /       \| $$__| $$
-	| $$  | $$| $$$$$$$\|  $$$$$$$| $$    $$
-	| $$  | $$| $$  | $$ \$$    \ | $$$$$$$\
-	| $$__/ $$| $$  | $$ _\$$$$$$\| $$  | $$
-	| $$    $$| $$  | $$|       $$| $$  | $$
-	 \$$$$$$$  \$$   \$$ \$$$$$$$  \$$   \$$
-                                        """
-	print(banner)
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-b","--blacklist",required=False,metavar="",help="Delete wildcard Cname or A records")
-	ap.add_argument("-o","--output",required=False,metavar="",help="Save output")
-	args = vars(ap.parse_args())
-	main()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-l","--list", required=False, metavar="", help="Read Targets From List")
+    ap.add_argument("-s","--stdin", required=False, action="store_true", help="Read Targets From Stding")
+    ap.add_argument("-o","--output", required=False, metavar="", help="Save output")
+    ap.add_argument("-t", "--thread", required=False, metavar="",default=20, type=int, help="Thread Number")
+    args = ap.parse_args()
+
+    print_lock = threading.Lock()
+
+    main()
